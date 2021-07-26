4e980bb7f7eefc484cf064d305d5fb15a1e6552b
Adrian Jules Sales <ajsales@berkeley.edu>
2021-07-26 01:34:17 -0700

    Finished view image screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+
 import WelcomeScreen from './screens/WelcomeScreen';
+import ViewImageScreen from './screens/ViewImageScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WelcomeScreen />
+      <ViewImageScreen />
       <StatusBar style="auto" />
     </View>
   );
